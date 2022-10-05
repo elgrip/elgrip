e04cca281ad8e84b5424783ae2503392bcbcb678
daniel001-odii <xenithheight@gmail.com>
2022-10-05 01:15:03 +0100

    build second page

---
+++ script.js
@@ -0,0 +1,18 @@
+
+/*
+const min = document.getElementById("minimize");
+const max = document.getElementById("maximize");
+const close = document.getElementById("close");
+
+
+var SPD_window = {head:document.getElementById("window-header"),
+body:document.getElementById("window-content")}
+
+min.onclick = function(){
+    SPD_window.body.style.display = "none";
+    // SPD_window.head.style.width = "300px";
+    SPD_window.head.style.position = "absolute";
+    SPD_window.head.style.bottom = "5px";
+    SPD_window.head.style.right = "5px";
+
+}*/
