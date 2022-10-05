6b9a09e9a84813b3ae1ea02c3f1231ed7a882d57
daniel001-odii <xenithheight@gmail.com>
2022-10-05 17:17:09 +0100

    make ui usable

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ min.onclick = function(){
     SPD_window.head.style.right = "5px";
 
 }*/
+
+//TOGGLING NESTED ul
+$(function() {
+    var list = $('.js-dropdown-list');
+    var link = $('.js-link');
+    link.click(function(e) {
+      e.preventDefault();
+      list.slideToggle(200);
+    });
+    list.find('li').click(function() {
+      var text = $(this).html();
+      var icon = '<i class="fa fa-chevron-down"></i>';
+      link.html(text+icon);
+      list.slideToggle(200);
+      if (text === '* Reset') {
+        link.html('Select one option'+icon);
+      }
+    });
+  });
